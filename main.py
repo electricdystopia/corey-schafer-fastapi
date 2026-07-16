790a33f0756e02f35cd7faba345027e9d20f6c73
electricdystopia <b22ai058@gmail.com>
2026-07-17 03:00:54 +0530

    jinja2 templates

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 posts: list[dict] = [
     {
@@ -20,12 +25,12 @@ posts: list[dict] = [
     },
 ]
 
-@app.get("/",response_class=HTMLResponse) #decorator
-@app.get("/posts")
-def home():
-    # return {"message": "Hello Worlddddd!"} #api endpoint returning documentation
-    return f"<h1>{posts[0]['title']}</h1>"
+@app.get("/",include_in_schema=False,name="home") #decorator
+@app.get("/posts",include_in_schema=False,name="posts")
+def home(request: Request):
+    return templates.TemplateResponse(request, "home.html", {"posts":posts, "title": "Home"})
 
-@app.get("/api/post") 
+@app.get("/api/posts")
 def get_posts():
-    return {"data" : posts}    
+    return posts 
+
